c9012e6b50a721514fbf4afb3ad575c5e1b4a0f6
jrfrobles <74306530+jrfrobles@users.noreply.github.com>
2025-10-01 14:35:04 +0800

    Feature: Added desc/asc functions

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -5,12 +5,16 @@ class BasketCaseController < ApplicationController
   def index; end
 
   def order_by_fruit
-    @list_items = @list_items.sort
+    @list_items = @list_items.sort_by {|fruit, _| fruit.to_s}
+    @list_items = @list_items.reverse! if params.has_key?(:order) && params[:order] == "desc"
+    @sorted_by = "name" 
     render 'basket_case/index'
   end
 
   def order_by_amount
-    @list_items = @list_items.sort_by(&:last)
+    @list_items = @list_items.sort_by{|_, amount| amount}
+    @list_items = @list_items.reverse! if params.has_key?(:order) && params[:order] == "desc"
+    @sorted_by = "amount" 
     render 'basket_case/index'
   end
 
